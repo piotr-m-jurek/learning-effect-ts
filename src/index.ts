cb06538e1e5c16211477ec4b66c4f5dd762a3b66
Piotr Malecki-Jurek <piotr.m.jurek@checkhq.com>
2025-08-21 20:41:22 +0200

    provided and happy

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,31 @@
-import * as Effect from 'effect'
+import { Effect } from 'effect'
 
 // TODO: wrap in a dependency
 import dotenv from 'dotenv'
 import { PokeApi} from './PokeApi.js'
+import { PokemonCollection } from './PokemonCollection.js'
+import { BuildPokeApiUrl } from './BuildPokeApiUrl.js'
 dotenv.config({quiet: true})
 // ====
 
-const program = Effect.Effect.gen(function* () {
+const program = Effect.gen(function* () {
     const pokeApi = yield* PokeApi;
     return yield* pokeApi.getPokemon;
 })
 
-const runnable = program.pipe(Effect.Effect.provideService(PokeApi, PokeApi.Live))
+const runnable = program.pipe(
+    Effect.provideService(PokeApi, PokeApi.Live),
+    Effect.provideService(PokemonCollection, PokemonCollection.Live),
+    Effect.provideServiceEffect(BuildPokeApiUrl, BuildPokeApiUrl.Live),
+)
 
 const main = runnable.pipe(
-    Effect.Effect.catchTags({
-        FetchError: () => Effect.Effect.succeed("There was a fetch error"),
-        JsonError: () => Effect.Effect.succeed("There was a json error"),
-        ResponseNotOk: () => Effect.Effect.succeed("Response returned not ok"),
-        ParseError: (e) => Effect.Effect.succeed(`There was an error when parsing the structure: ${e.toString()}`)
+    Effect.catchTags({
+        FetchError: () => Effect.succeed("There was a fetch error"),
+        JsonError: () => Effect.succeed("There was a json error"),
+        ResponseNotOk: () => Effect.succeed("Response returned not ok"),
+        ParseError: (e) => Effect.succeed(`There was an error when parsing the structure: ${e.toString()}`)
     })
 )
 
-Effect.Effect.runPromise(main).then(console.log)
+Effect.runPromise(main).then(console.log)
