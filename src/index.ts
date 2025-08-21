6b7c4436fa9264f6e735528f49b0f082ceaa42ab
Piotr Malecki-Jurek <piotr.m.jurek@checkhq.com>
2025-08-21 18:39:30 +0200

    new errors, response not ok etc

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { Console, Data, Effect, } from 'effect'
 
-class FetchError extends Data.TaggedError("FetchError") {}
+class FetchError extends Data.TaggedError("FetchError")<{message?: string}> {}
 class JsonError extends Data.TaggedError("JsonError") {}
+class ResponseNotOk extends Data.TaggedError("ResponseNotOk") {}
+
+const makeError = (e: unknown) => e instanceof Error ? e : new Error(String(e))
 
 const fetchRequest = Effect.tryPromise({
     try: () => fetch("https://pokeapi.co/api/v2/pokemon/garchomp/"),
-    catch: () => new FetchError()
+    catch: (e) => new FetchError({ message: makeError(e).message })
 })
 
 const jsonRespnse = (response: Response) => 
@@ -17,12 +20,13 @@ const jsonRespnse = (response: Response) =>
 const main = fetchRequest.pipe(
     Effect.filterOrFail(
         response => response.ok,
-        () => new FetchError()
+        () => new ResponseNotOk()
     ),
     Effect.flatMap(jsonRespnse),
     Effect.catchTags({
         FetchError: () => Effect.succeed("There was a fetch error"),
-        JsonError: () => Effect.succeed("There was a json error")
+        JsonError: () => Effect.succeed("There was a json error"),
+        ResponseNotOk: () => Effect.succeed("Response returned not ok")
     })
 )
 
