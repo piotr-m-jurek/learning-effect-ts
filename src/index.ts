c6d7956f111f86cae7f84207181cb0d77f708dc7
Piotr Malecki-Jurek <piotr.m.jurek@checkhq.com>
2025-08-21 18:15:56 +0200

    effect version - unhandled errors

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import { Console, Effect } from 'effect'
 
-const fetchRequest = () => fetch("https://pokeapi.co/api/v2/pokemon/garchomp/")
+const fetchRequest = Effect.promise(
+    () => fetch("https://pokeapi.co/api/v2/pokemon/garchomp/")
+)
+const jsonRespnse =(response: Response) =>Effect.promise(() =>    response.json())
+const main = Effect.flatMap(
+    fetchRequest,
+    jsonRespnse
+)
 
-const jsonRespnse = (response: Response) =>  response.json()
-const main = async () => {
-    const response = await fetchRequest()
-    const json = await jsonRespnse(response)
 
-    return json
-}
-
-main().then(console.log)
+Effect.runPromise(main).then(console.log)
